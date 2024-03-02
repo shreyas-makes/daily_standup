aee1f0fc94d3e76cd968be9e86bf0c4cf65f3d8c
Shreyas <shreyas314159@gmail.com>
2024-03-02 08:54:29 +0000

    rendered a broken view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Application } from "hotwired/stimulus"
+import Dropdown from 'stimulus-dropdown'
+
+const application = Application.start ()
+application. register( 'dropdown', Dropdown)
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
---
+++ spec/views/activity/feed.html.tailwindcss_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "activity/feed.html.tailwindcss", type: :view do
+  it "renders the word feed" do
+    render template: 'activity/feed'
+    expect(rendered) .to match (/feed/)
+  end
+end
+
+
